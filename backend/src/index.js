f696a405f33d3e8a249fa384a088f7ff52510eb0
Vansh Vadhwa <vvad0647@gmail.com>
2024-12-10 15:42:48 +0530

    Fifth Commit(Models are done as of now, controllers for the next commit)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -19,5 +20,6 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at http://localhost:${PORT}`);
 });
